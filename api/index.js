cd69603aa581d427342e748f18aa446c0e83a037
Akshita394 <akshitafulare394@gmail.com>
2025-02-10 19:16:24 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose';
 import colors from "colors";
 import dotenv from 'dotenv';
+
+dotenv.config();
 mongoose.connect(
     process.env.MONGO
     )
