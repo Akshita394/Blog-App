2d3d0fb238b32e50427500b62867fcc0dded47e6
Akshita394 <akshitafulare394@gmail.com>
2025-02-10 19:32:07 +0530

    create  atest api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import colors from "colors";
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 
 dotenv.config();
 mongoose.connect(
@@ -21,3 +22,5 @@ const app = express();
 app.listen (3000,() => {
     console.log('server is running on port 3000'.bgCyan.white)
 });
+
+app.use('/api/user',userRoutes);
