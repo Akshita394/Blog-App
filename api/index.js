49e705b755721b22e4d1d495d35fbf27408d22a5
Akshita394 <akshitafulare394@gmail.com>
2025-02-14 23:23:03 +0530

    importing cors and handling errors

--- api/index.js
+++ api/index.js
@@ -4,9 +4,12 @@ import colors from "colors";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cors from 'cors';
 
 dotenv.config();
-mongoose.connect(
+
+mongoose
+    .connect(
     process.env.MONGO
     )
     .then( () =>{
@@ -19,10 +22,15 @@ mongoose.connect(
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
 app.use(express.json());
+
 app.listen (3000,() => {
-    console.log('server is running on port 3000'.bgCyan.white)
+    console.log('server is running on port 3000!'.bgCyan.white)
 });
+
+
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
@@ -33,6 +41,6 @@ app.use((err,req,res,next) => {
     res.status(statusCode).json({
         success: false,
         statusCode,
-        message
+        message,
     })
 })
