f085e722a574a7733197119142c3aa1349488496
Akshita394 <akshitafulare394@gmail.com>
2025-03-05 19:52:06 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -25,6 +26,8 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen (3000,() => {
     console.log('server is running on port 3000!'.bgCyan.white)
